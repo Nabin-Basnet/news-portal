04b85035bb7811fddae3a1339d7c3b40ae0d1f0c
Nabin-Basnet <nabinbasnet1200@gmail.com>
2026-06-06 14:21:44 +0545

    Add auto admin creation script

--- create_admin.py
+++ create_admin.py
@@ -7,13 +7,13 @@ django.setup()
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-# Change these to whatever credentials you want to use
 username = "admin"
-email = "basnet@gmail.com"
-password = "your_secure_password"
+email = "user@gmail.com"  # The email you are trying to use
+password = "user123"  # Set a secure password here
 
-if not User.objects.filter(username=username).exists():
+# Checks if the user exists; if not, creates a fully privileged admin
+if not User.objects.filter(email=email).exists() and not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
-    print("Superuser created successfully!")
+    print("--- Superuser created successfully! ---")
 else:
-    print("Superuser already exists.")
+    print("--- Superuser already exists. ---")
