e75a463a080e924b50b39ff7a72074994071c184
Nabin-Basnet <nabinbasnet1200@gmail.com>
2026-06-09 20:50:30 +0545

    deploy fix

--- create_admin.py
+++ create_admin.py
@@ -7,13 +7,16 @@ django.setup()
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-username = "admin"
-email = "user@gmail.com"  # The email you are trying to use
-password = "user123"  # Set a secure password here
 
-# Checks if the user exists; if not, creates a fully privileged admin
-if not User.objects.filter(email=email).exists() and not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
+email = "user@gmail.com"
+password = "user123"
+
+# Create admin if it doesn't already exist
+if not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(
+        email=email,
+        password=password
+    )
     print("--- Superuser created successfully! ---")
 else:
     print("--- Superuser already exists. ---")
